5b34453bdc5c576447d7d9c73a4c35127fdaaf2c
Omar Padron <omar.padron@kitware.com>
2016-03-02 17:36:10 -0500

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+
+var path = require("path");
+
+var webpack = require("webpack");
+
+module.exports = {
+    cache: true,
+
+    entry: "./main",
+    context: path.resolve(__dirname, "web-external", "src"),
+
+    output: {
+        path: path.resolve(__dirname, "web-external", "lib"),
+        filename: "bundle.js"
+    },
+
+
+    module: {
+        loaders: [
+            { test: /\.css$/,
+              loader: "style-loader!css-loader" },
+
+            { test: /\.styl$/,
+              loader: "style-loader!css-loader!stylus-loader" },
+
+            { test: /\.jade$/,
+              loader: "jade" },
+
+            // required for babel
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                query: { presets: ["react", "es2015"] }
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [".js", ".styl", ".css", ".jade", ""],
+        modulesDirectories: ["./web-external/src", "./node_modules"]
+    },
+
+    plugins: [
+        new webpack.ProvidePlugin({
+            // Automtically detect jQuery and $ as free var in modules
+            // and inject the jquery library
+            // This is required by many jquery plugins
+            jQuery: "jquery",
+            $: "jquery"
+        })
+    ]
+};
+
