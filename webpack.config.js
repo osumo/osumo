3b8ba8c15b69f22bce07d931ac7345acf615a4f2
Omar Padron <omar.padron@kitware.com>
2016-03-03 11:30:29 -0500

    add dependencies
      - babel-plugin-transform-runtime
      - babel-polyfill
      - babel-preset-stage-0
      - babel-runtime
      - backbone

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,10 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
-                query: { presets: ["react", "es2015"] }
+                query: {
+                    plugins: ["transform-runtime"],
+                    presets: ["es2015", "stage-0", "react"]
+                }
             }
         ]
     },
