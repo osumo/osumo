754c51c69c67df73dd5b1c55768b08b6f7530cc9
Omar Padron <omar.padron@kitware.com>
2016-03-04 17:01:53 -0500

    update main application code

--- web-external/src/components/mainApp.js
+++ web-external/src/components/mainApp.js
@@ -1,61 +1,94 @@
 
 import React from "react";
 
-import Header from "./headerContainer";
-import GlobalNav from "./globalNavContainer";
-import Footer from "./footerContainer";
+import Header from "./header";
+import GlobalNav from "./globalNav";
+import Footer from "./footer";
 
-class DummyBodyComponent extends React.Component {
-    render() {
-        return <div id="g-app-body-container" className="g-default-layout"/>;
-    }
-}
+const DummyBodyComponent = () => (
+    <div id="g-app-body-container" className="g-default-layout"/>
+);
 
 export default class MainApp extends React.Component {
-
     static defaultProps = {
         apiRoot: "api/v1",
-        staticRoot: "static",
+        currentTarget: "",
         currentUser: null,
-        navItems: [],
-        navMap: {},
+        navList: [],
+        navTable: {},
+        staticRoot: "static",
+
         /* TODO(opadron): replace with a warning message */
-        onNavigate: (route) => console.log("ROUTE: " + route)
+        onCollections() { console.log("ON COLLECTIONS" ); },
+        onQuickSearch() { console.log("ON QUICK SEARCH"); },
+        onFolders()     { console.log("ON FOLDERS"     ); },
+        onInfo()        { console.log("ON INFO"        ); },
+        onRegister()    { console.log("ON REGISTER"    ); },
+        onLogin()       { console.log("ON LOGIN"       ); },
+        onNavigate()    { console.log("ON NAVIGATE"    ); },
+        onTitle()       { console.log("ON TITLE"       ); }
     };
 
-    state = {};
-
     render() {
-        var BodyComponent = (
-            this.props.navMap[this.state.navKey] || DummyBodyComponent);
+        let {
+            apiRoot,
+            currentTarget,
+            currentUser,
+            navList,
+            navTable,
+            staticRoot,
 
-        var body = (
-            BodyComponent === DummyBodyComponent
-          ? <BodyComponent/>
-          : <BodyComponent key={ this.state.navKey }
-                           apiRoot={ this.props.apiRoot }
-                           staticRoot={ this.props.staticRoot }
-                           onNavigate={ this.props.onNavigate }
-                           currentUser={ this.props.currentUser }/>);
+            onCollections,
+            onFolders,
+            onInfo,
+            onLogin,
+            onLogout,
+            onNavigate,
 
-        /*
-         * NOTE(opadron): We can probably do away with the other views/revisit
-         *                them later.
-         */
+            onQuickSearch,
+            onRegister,
+            onTitle
+        } = this.props;
+
+        let Body = navTable[currentTarget] || DummyBodyComponent;
+
         return (
             <div>
-              <Header currentUser={ this.props.user }
-                      onNavigate={ this.props.onNavigate }/>
-              <GlobalNav navItems={ this.props.navItems }
-                      onNavigate={ this.props.onNavigate }
-                      activeItem={ this.state.navKey }/>
-              { body }
-              <Footer apiRoot="api/v1"/>
-              <div id="g-app-progress-container"/>
-              <div id="g-dialog-container" className="modal fade"/>
-              <div id="g-alerts-container"/>
+              <Header currentUser={ currentUser }
+                      onFolders={ onFolders }
+                      onInfo={ onInfo }
+                      onLogout={ onLogout }
+                      onLogin={ onLogin }
+                      onRegister={ onRegister }
+                      onTitle={ onTitle }/>
+
+              <GlobalNav currentTarget={ currentTarget }
+                         navList={ navList }
+                         onNavigate={ onNavigate }/>
+
+              <Body key={ currentTarget }
+                    apiRoot={ apiRoot }
+                    staticRoot={ staticRoot }
+                    currentUser={ currentUser }
+                    onCollections={ onCollections }
+                    onFolders={ onFolders }
+                    onInfo={ onInfo }
+                    onLogin={ onLogin }
+                    onNavigate={ onNavigate }
+                    onQuickSearch={ onQuickSearch }
+                    onRegister={ onRegister }/>
+
+              <Footer apiRoot={ apiRoot }/>
             </div>
         );
+
+        /*
+         * NOTE(opadron): We can probably do away with these views/revisit them
+         *                later.
+         */
+        // <div id="g-app-progress-container"/>
+        // <div id="g-dialog-container" className="modal fade"/>
+        // <div id="g-alerts-container"/>
     }
 }
 
--- web-external/src/main.js
+++ web-external/src/main.js
@@ -1,18 +1,18 @@
 
+import "babel-polyfill";
+
 import $ from "jquery";
 import React from "react";
 import ReactDOM from "react-dom";
-import { partial, isArray } from "underscore";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import { partial, isArray } from "underscore";
+
+import MainApp from "./components/mainApp";
 import FrontPage from "./components/frontPage";
-import App from "./components/mainApp";
-import styles from "./components/fullViewPort";
 
-import router from "./router";
-import rootReducer from "./reducer";
-import restRequests from "./utils/restRequests";
-import events from "./utils/events";
+import styles from "./style/fullViewPort";
 
 const DummyComponent = ({ msg }) => (
     <div id="g-app-body-container" className="g-default-layout">
@@ -20,25 +20,32 @@ const DummyComponent = ({ msg }) => (
     </div>);
 
 $(() => {
+    /* define some constants here */
+    /* TODO(opadron): Is there a better place for these? */
+    const apiRoot = "api/v1";
+    const staticRoot = "static";
+
+    /* create store and populate it with initial state */
+    let { default: rootReducer } = require("./reducer");
+    const store = createStore(rootReducer);
 
     const dummy = (msg) => partial(DummyComponent, { msg });
-    const store = createStore(rootReducer);
 
     store.dispatch({
         type: "ADD_GLOBAL_NAV",
         items: [
-            /* [name]       [icon]     [target]    */
-            ["Collections", "sitemap", "collections"],
-            ["Users"      , "user"   , "users"      ],
-            ["INDEX"      , "user"   , ""           ],
-            ["Groups"     , "users"  , "groups"     ],
-            ["INDEX"      , "user"   , ""           ]
-        ].map(([name, icon, target]) => ({ name, icon, target }))
+            { name: "Collections", icon: "sitemap", target: "collections" },
+            { name: "Users"      , icon: "user"   , target: "users"       },
+            { name: "INDEX"      , icon: "user"   , target: ""            },
+            { name: "Groups"     , icon: "users"  , target: "groups"      },
+            { name: "INDEX"      , icon: "user"   , target: ""            }
+        ]
     });
 
     store.dispatch({
         type: "SET_GLOBAL_NAV_TARGET",
         mappings: {
+            /* [target]: [component] */
             ""         : FrontPage,
             collections: dummy("Collections Test"),
             users      : dummy("Users Test"      ),
@@ -46,15 +53,16 @@ $(() => {
         }
     });
 
-    let $div;
-    $(document.body).append($div = $("<div>").attr("id", "root"));
+    /* initialize rest API */
+    let { default: events } = require("./utils/events");
+    let { default: restRequests } = require("./utils/restRequests");
 
-    let route = router();
-    let onNavigate = path => route(path);
+    const rest = restRequests({ events, apiRoot });
 
-    let app;
+    /* define some function to be used for routing */
 
-    const setTarget = ({ params: { target } }, next) => {
+    /* :target/a/b/c/etc... -> sets global nav target */
+    const setGlobalNavTarget = ({ params: { target } }, next) => {
         store.dispatch({
             type: "SET_CURRENT_GLOBAL_NAV_TARGET",
             target
@@ -62,16 +70,18 @@ $(() => {
         next();
     };
 
-    const REST = restRequests({
-        events,
-        apiRoot: "api/v1"
-    });
+    /* like above, but hardcoded for the main page */
+    const setGlobalNavTargetToIndex = (_, next) => setGlobalNavTarget(
+        { params: { target: "" } },
+        next
+    );
 
-    const handleSpecialTarget = ({ params: { target } }, next) => {
+    /* :target/... -> checks target for special values before passing along */
+    const handleSpecialNavTarget = ({ params: { target } }, next) => {
         if(target === "login") {
             console.log("LOGIN");
         } else if(target === "logout") {
-            REST.logout();
+            rest.logout();
         } else if(target === "register") {
             console.log("REGISTER");
         } else {
@@ -79,19 +89,61 @@ $(() => {
         }
     };
 
-    let targetMiddleWare = [handleSpecialTarget,
-                            setTarget];
+    let targetMiddleWare = [
+        handleSpecialNavTarget,
+        setGlobalNavTarget
+    ];
 
-    route("", (_, next) => setTarget({ params: { target: "" } }, next));
+    /* create the router, mount middleware, and start routing */
+    let { default: router } = require("./utils/router");
+    let route = router();
 
+    route("", setGlobalNavTargetToIndex);
     route(":target", ...targetMiddleWare);
-    route(":target/:extra/*nested", ...targetMiddleWare, ({ params }) => {
-        console.log("NESTED ROUTE:")
-        console.log(params);
-    });
 
     route.base();
     route.pushState(false);
     route.start();
+
+    let $div;
+    $(document.body).append($div = $("<div>").attr("id", "root"));
+
+    /* create main application component and render */
+    let { default: reduxComponent } = require("./utils/redux-component");
+    let App = reduxComponent(
+        store,
+        (
+            {
+                globalNav: {
+                    currentTarget,
+                    list: navList,
+                    table: navTable
+                },
+                currentUser
+            },
+            dispatch
+        ) => (
+            <MainApp apiRoot="api/v1"
+                     currentTarget={ currentTarget }
+                     currentUser={ currentUser }
+                     navList={ navList }
+                     navTable={ navTable }
+                     staticRoot="static"
+
+                     onCollections={ () => route("collections") }
+                     onFolders={ (id) => route(`user/${ id }`) }
+                     onInfo={ (id) => route(`useraccount/${ id }/info`) }
+                     /* TODO(opadron): add a login handler */
+                     /* onLogin={ ... } */
+                     onLogout={ (id) => route(`useraccount/${ id }/logout`) }
+                     onNavigate={ (target) => route(target) }
+                     /* TODO(opadron): what to do about the search bar? */
+                     /* onQuickSearch={ ... } */
+                     onRegister={ () => route("register") }
+                     onTitle={ () => route("") }/>
+        )
+    );
+
+    ReactDOM.render(<App/>, $div[0]);
 });
 
