64689e01619dc5d725f5b7a72bab84083d20edd4
Omar Padron <omar.padron@kitware.com>
2016-03-04 16:23:12 -0500

    update webpack config
      - dynamically set production mode based on $NODE_ENV environment variable
      - new plugins:
        - webpack.optimize.UglifyJsPlugin
        - webpack.HashedModuleIdsPlugin
        - webpack.NamedModulesPlugin
        - webpack.optimize.CommonsChunkPlugin
      - break webpack bundle into mutliple files
        - lib/runtime.js -- webpack runtime
        - lib/common.js -- common and third-party libraries
        - lib/main.js -- main application code

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,81 @@
 
+"use strict";
+
 var path = require("path");
+var process = require("process");
 
 var webpack = require("webpack");
+
+var production = process.env.NODE_ENV === "production";
+var plugins = [];
+
+if(production) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false },
+            output: {
+                comments: false,
+                semicolons: true
+            }
+        })
+    );
+}
+
+plugins = plugins.concat([
+    new webpack.HashedModuleIdsPlugin({
+        hashFunction: "sha1",
+        hashDigestLength: 3
+    }),
+
+    new webpack.NamedModulesPlugin(),
+
+    new webpack.ProvidePlugin({
+        // Automtically detect jQuery and $ as free var in modules
+        // and inject the jquery library
+        // This is required by many jquery plugins
+        jQuery: "jquery",
+        $: "jquery"
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+        name: "common",
+        minChunks: 3
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+        name: "runtime",
+        chunks: ["common"],
+        minChunks: Infinity
+    })
+]);
+
+if(production) {
+    plugins.push(
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        })
+    );
+}
 
 module.exports = {
     cache: true,
 
+    devtool: (production ? "source-map" : null),
+
     entry: {
+        common: [
+            "backbone",
+            "bluebird",
+            "jquery",
+            "react",
+            "react-dom",
+            "react-redux",
+            "redux",
+            "underscore"
+        ],
+
         main: "./main"
     },
 
@@ -16,7 +85,6 @@ module.exports = {
         path: path.resolve(__dirname, "web-external", "lib"),
         filename: "[name].js"
     },
-
 
     module: {
         loaders: [
@@ -47,20 +115,6 @@ module.exports = {
         modulesDirectories: ["./web-external/src", "./node_modules"]
     },
 
-    plugins: [
-        new webpack.ProvidePlugin({
-            // Automtically detect jQuery and $ as free var in modules
-            // and inject the jquery library
-            // This is required by many jquery plugins
-            jQuery: "jquery",
-            $: "jquery"
-        }),
-
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production")
-            }
-        })
-    ]
+    plugins: plugins
 };
 
