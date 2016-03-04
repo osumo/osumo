1801bf4b8c71a5e5e9b11360fc11094180a17ffe
Omar Padron <omar.padron@kitware.com>
2016-03-04 16:14:54 -0500

    add redux-component module

---
+++ web-external/src/utils/redux-component.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+
+const reduxComponent = (store, renderCallback) => {
+    const dispatch = (...args) => store.dispatch(...args);
+
+    return class ReduxComponent extends React.Component {
+        componentDidMount() {
+            this.unsubscribe = store.subscribe(() =>
+                this.forceUpdate()
+            );
+        }
+
+        componentWillUnmount() {
+            this.unsubscribe();
+        }
+
+        render() {
+            return renderCallback(store.getState(), dispatch);
+        }
+    };
+};
+
+export default reduxComponent;
+
+
