af41c0324543371522c97af2db44a2ac87053fbe
David Manthey <david.manthey@kitware.com>
2016-03-15 11:23:34 -0400

    Use the girder worker temp directory for the temp png file.
    Change the css styling back to what Girder uses for the footer (it was hiding some data).

--- server/job_specs/surv.r
+++ server/job_specs/surv.r
@@ -5,7 +5,8 @@ k <- num_clusters
 cl <- kmeans(t(ge_filt), centers = k)
 fit <- survfit(Surv(surv$time, surv$censor) ~ as.factor(cl$cluster))
 sdf <- survdiff(Surv(surv$time, surv$censor) ~ as.factor(cl$cluster))
-png(filename=png_output_path, width=800, height=600, units="px", pointsize=8)
+localdir = Sys.getenv('tempdir', unset=tempdir())
+dataplot = tempfile('plot', tmpdir=tempdir, fileext='.png')
+png(filename=dataplot, width=800, height=600, units="px", pointsize=8)
 plot(fit, col=c(1:5), lwd=4, cex.axis=1.5, font.axis=2)
 dev.off()
-dataplot <- png_output_path
