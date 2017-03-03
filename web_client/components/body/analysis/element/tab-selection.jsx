5d8f7ccc665420e08d36cf55cb60f515ae837c44
chaitanya2334 <reaper.2334@gmail.com>
2017-03-02 20:54:53 -0500

    fixed some more travis lint errors

--- web_client/components/body/analysis/element/tab-selection.jsx
+++ web_client/components/body/analysis/element/tab-selection.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import ImageElement from './image';
-import { isUndefined } from 'lodash';
+
 
 class TabSelection extends React.Component {
   render () {
-    let { action, name, mainAction, onAction, items } = this.props;
-
-    action = action || mainAction;
-
-    let displayText = name;
+    let { name, mainAction, items } = this.props;
 
     return (
       <div>
         <ul className='nav nav-tabs'>
-          {items.map((item, index) => (<li className={index===0? 'active': ''}><a data-toggle='tab' href={'#' + index}>{item.name}</a></li>))}
+          {items.map((item, index) => (<li className={index === 0 ? 'active': ''}><a data-toggle='tab' href={'#' + index}>{item.name}</a></li>))}
         </ul>
         <div className='tab-content'>
           {items.map((item, index) => (
 
-            <div id={index} className={ index===0? 'tab-pane fade in active': 'tab-pane fade'}>
+            <div id={index} className={index === 0? 'tab-pane fade in active': 'tab-pane fade'}>
               <div className='task-notes'>{ item.notes }</div>
               <ImageElement key='result' fileId={item.fileId} />
             </div>
@@ -31,10 +27,8 @@ class TabSelection extends React.Component {
 
   static get propTypes () {
     return {
-      action: React.PropTypes.string,
       name: React.PropTypes.string,
       mainAction: React.PropTypes.string,
-      onAction: React.PropTypes.func,
       items: React.PropTypes.arrayOf(React.PropTypes.Object)
     };
   }
