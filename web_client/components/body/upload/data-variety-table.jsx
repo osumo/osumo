4b6b42860e656efa97cd272bf4cef76d4b5d6478
chaitanya2334 <chaitanya2334@users.noreply.github.com>
2017-03-08 23:59:32 -0500

    Update data-variety-table.jsx
    added 'somatic mutation' and 'imaging and morphological parameters'.

--- web_client/components/body/upload/data-variety-table.jsx
+++ web_client/components/body/upload/data-variety-table.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 export const table = [
-  { id: 'proteinX'    , description: 'Protein Expression'                },
-  { id: 'rawSequence' , description: 'Raw Sequencing Data'               },
-  { id: 'cprofile'    , description: 'Clinical Profile'                  },
-  { id: 'dnaMeth'     , description: 'DNA Methylation'                   },
-  { id: 'mrna'        , description: 'Gene Expression Quantification'    },
-  { id: 'mirna'       , description: 'MicroRNA Quantification'           },
-  { id: 'isoX'        , description: 'Isoform Expression Quantification' },
-  { id: 'exonJunction', description: 'Exon Junction Quantification'      },
-  { id: 'exon'        , description: 'Exon Quantification'               },
-  { id: 'mrnaSummary' , description: 'Gene Expression Summary'           },
+  { id: 'proteinX'    , description: 'Protein Expression'                   },
+  { id: 'rawSequence' , description: 'Raw Sequencing Data'                  },
+  { id: 'cprofile'    , description: 'Clinical Profile'                     },
+  { id: 'dnaMeth'     , description: 'DNA Methylation'                      },
+  { id: 'mrna'        , description: 'Gene Expression Quantification'       },
+  { id: 'mirna'       , description: 'MicroRNA Quantification'              },
+  { id: 'isoX'        , description: 'Isoform Expression Quantification'    },
+  { id: 'exonJunction', description: 'Exon Junction Quantification'         },
+  { id: 'exon'        , description: 'Exon Quantification'                  },
+  { id: 'mrnaSummary' , description: 'Gene Expression Summary'              },
+  { id: 'som'         , description: 'Somatic Mutation'                     },
+  { id: 'img'         , description: 'Imaging and morphological parameters' },
 
   /* should always have a none option */
-  { id: 'none'        , description: ' - none/other - '                  }
+  { id: 'none'        , description: ' - none/other - '                      }
 ];
 
 export const elements = table.map(
